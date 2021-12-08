ca99da2177e5e0194b80b136ee36b60c4660e2de
Aryan Ritesh <aryanrtsh@gmail.com>
2021-12-08 23:26:23 +0530

    Repo is ready

--- app/src/main/java/blehblu/com/NoteDatabase.java
+++ app/src/main/java/blehblu/com/NoteDatabase.java
@@ -1,10 +1,14 @@
 package blehblu.com;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
 @Database(entities =note.class, version = 1)
 public abstract class NoteDatabase extends RoomDatabase {
          private static NoteDatabase instance;
@@ -12,10 +16,30 @@ public abstract class NoteDatabase extends RoomDatabase {
          public static synchronized  NoteDatabase getInstance(Context context){
              if(instance==null){
                  instance= Room.databaseBuilder(context.getApplicationContext(), NoteDatabase.class, "note_database")
-                         .fallbackToDestructiveMigration().build();
+                         .fallbackToDestructiveMigration().addCallback(roomCallback).build();
              }
                 return instance;
 
+         }
+         private static RoomDatabase.Callback roomCallback= new Callback() {
+             @Override
+             public void onCreate(@NonNull SupportSQLiteDatabase db) {
+                 super.onCreate(db);
+                 new PopulateDbAsyncTask(instance).execute();
+             }
+         };
+         private static class PopulateDbAsyncTask extends AsyncTask<Void,Void,Void>{
+            private noteDAO noteDAO;
+            private PopulateDbAsyncTask(NoteDatabase database){
+                noteDAO=database.noteDAO();
+            }
+             @Override
+             protected Void doInBackground(Void... voids) {
+                noteDAO.insert(new note("Title 1","Description 1"));
+                noteDAO.insert(new note("Title 2","Description 2"));
+                 return null;
+
+             }
          }
 
 }
--- app/src/main/java/blehblu/com/noteRepo.java
+++ app/src/main/java/blehblu/com/noteRepo.java
@@ -1,8 +1,10 @@
 package blehblu.com;
 
 import android.app.Application;
+import android.os.AsyncTask;
 
 import androidx.lifecycle.LiveData;
+import androidx.loader.content.AsyncTaskLoader;
 
 import java.util.List;
 
@@ -12,6 +14,58 @@ public class noteRepo {
     public noteRepo(Application application){
         NoteDatabase database=NoteDatabase.getInstance(application);
         noteDAO= database.noteDAO();
+        notes=noteDAO.getAllNotes();
     }
 
+    public void insert(note note){
+     new InsertNoteAsync(noteDAO).execute(note);
+    }
+    public void update(note note){
+     new UpdateNoteAsync(noteDAO).execute(note);
+    }
+    public void delete(note note){
+     new DeleteNoteAsync(noteDAO).execute(note);
+    }
+    public  LiveData<List<note>> getAllNotes(){
+        return notes;
+    }
+
+    public  static class InsertNoteAsync extends AsyncTask<note,Void,Void> {
+        private noteDAO noteDAO;
+        private InsertNoteAsync(noteDAO noteDAO){
+            this.noteDAO=noteDAO;
+        }
+        @Override
+        protected Void doInBackground(note... notes) {
+            noteDAO.insert(notes[0]);
+            return null;
+
+        }
+    }
+
+    public  static class UpdateNoteAsync extends AsyncTask<note,Void,Void> {
+        private noteDAO noteDAO;
+        private UpdateNoteAsync(noteDAO noteDAO){
+            this.noteDAO=noteDAO;
+        }
+        @Override
+        protected Void doInBackground(note... notes) {
+            noteDAO.update(notes[0]);
+            return null;
+
+        }
+    }
+
+    public  static class DeleteNoteAsync extends AsyncTask<note,Void,Void> {
+        private noteDAO noteDAO;
+        private DeleteNoteAsync(noteDAO noteDAO){
+            this.noteDAO=noteDAO;
+        }
+        @Override
+        protected Void doInBackground(note... notes) {
+            noteDAO.delete(notes[0]);
+            return null;
+
+        }
+    }
 }
